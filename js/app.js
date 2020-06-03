ad449544aced4f18be85e133fe12c79fb19f994f
Rene Montoya <rmontoya.we@gmail.com>
2020-06-02 17:05:56 -0700

    finishing before the stretch goals

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // The program will have 3 yes/ questions: as a cf student do I live in WA? do I know another language aside from English? Do I enjoy the cold?
 var myState = prompt('As a CodeFellows student, do I live in Washington?' );
@@ -7,6 +7,7 @@ if (myState === 'no'||myState === 'No' || myState === 'N' || myState === 'n'){
 } else {
   alert('Nope, I am attending remotely from CA');
 }
+console.log('I am from CA, you said ' + myState);
 
 var whatSpeak = prompt('Does the developer speak a language other than English?');
 var whatSpeakRecase = whatSpeak.toLowerCase();
@@ -17,6 +18,7 @@ if (whatSpeakRecase === 'yes' || whatSpeakRecase === 'y'){
 } else {
   whatSpeak = prompt('Please type yes or no');
 }
+console.log('I speak Spanish and Japanese, you said: ' + whatSpeak);
 
 var myTemp = prompt('Based on the first question, do I enjoy the cold?');
 var myTempCase = myTemp.toLowerCase();
@@ -28,22 +30,23 @@ if (myTempCase === 'yes' || myTempCase === 'y') {
 } else {
   whatSpeak = prompt('Please type yes or no');
 }
-
-// the other questions will be multiple choice: I majored in a STEM subject, quess which one. Based on the previous question, did I receive a bachelors, masters, both, or neither? 
+console.log('I hate the cold, you said ' + myTemp);
+// the other questions will be multiple choice: I majored in a STEM subject, quess which one. Based on the previous question, did I receive a bachelors, masters, both, or neither?
 var myMajor = prompt('I majored in a STEM subject, which do you think?');
 var myMajorCase = myMajor.toLowerCase();
 
-if (myMajorCase === 'mathematics' && myMajorCase === 'math' && myMajorCase === 'm') {
+if (myMajorCase === 'mathematics' || myMajorCase === 'math' || myMajorCase === 'm') {
   alert('Yup! I loved it so much I continue to do math this day! See how in my "About Me"!');
-} else if (myMajorCase === 'technology' && myMajorCase === 'tech' && myMajorCase === 't') {
+} else if (myMajorCase === 'technology' || myMajorCase === 'tech' || myMajorCase === 't') {
   alert('Not quite. Check my "About Me" to find out!');
-} else if (myMajorCase === 'science' && myMajorCase === 's') {
+} else if (myMajorCase === 'science' || myMajorCase === 's') {
   alert('No, but my major is involved in a lot of sciences. Check my "About Me"');
-} else if (myMajorCase === 'engineering' && myMajorCase === 'e') {
+} else if (myMajorCase === 'engineering' || myMajorCase === 'e') {
   alert('I started as that but the physics killed me so I switched. Check my "About Me"!');
 } else {
   alert('See my "About Me" to find out!');
 }
+console.log('I studied math, you said: ' + myMajor);
 
 var myEducation = prompt('Did I receive a bachelors, masters, both, or neither?');
 var myEducationCase = myEducation.toLowerCase();
@@ -55,3 +58,13 @@ if (myEducationCase === 'both') {
 } else {
   alert('Just check my "About Me".');
 }
+console.log('I have a BA and MA, you said: ' + myEducation);
+
+
+//now for the personalization. The code will ask the user their name and display a message to them
+
+var userName = prompt('what is your name?');
+while (userName ===''|| userName === null){
+  userName = prompt('Please input a character for your name or else!');
+}
+alert('Welcome to my page ' + userName + ' , I hope you enjoy this page :)');
